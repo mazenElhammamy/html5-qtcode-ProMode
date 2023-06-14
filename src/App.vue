8a69837597f7ebff618393b672211c2412c49e47
Mazen Elhammamy <mazene@topdesk.com>
2023-06-14 10:58:11 +0200

    show the scaned qr

--- src/App.vue
+++ src/App.vue
@@ -10,14 +10,20 @@
           @result="onScan"
         />
       </div>
+      <div>
+        {{ result }}
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import QRScanner from "./components/QRCodeSanner.vue";
 
+const result = ref("");
 function onScan(decodedText: string) {
+  result.value = decodedText;
   console.log(decodedText);
 }
 </script>
